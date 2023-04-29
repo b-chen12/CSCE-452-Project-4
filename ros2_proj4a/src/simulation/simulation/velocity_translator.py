96b2d0deed9522f9f4a7a03776571ed5917640b5
luckstrike <alexisgarc711@gmail.com>
2023-04-28 23:32:26 -0500

    Added in untested code to velocity_translator

--- ros2_proj4a/src/simulation/simulation/velocity_translator.py
+++ ros2_proj4a/src/simulation/simulation/velocity_translator.py
@@ -17,3 +17,34 @@ class VelocityTranslator(Node):
     def __init__(self):
         super.__init__('VelocityTranslator')
 
+        self.vel_sub = self.create_subscription(Twist, '/cmd_vel', self.listener_callback, 10)
+
+        self.vl_pub = self.create_publisher(Float64, '/vl')
+        self.vr_pub = self.create_publisher(Float64, '/vr')
+
+        self.wheel_dist = None # distance between wheels, need to figure out how to get this
+
+    def listener_callback(self, msg):
+        linear_x = msg.linear.x
+        angular_z = msg.angular.z
+
+        vl = linear_x - (self.wheel_dist / 2) * angular_z # calculates vl
+        vr = linear_x + (self.wheel_dist / 2) * angular_z # calculates vr
+
+        # publishes out the calculated data
+        self.vl_pub.publish(Float64(vl))
+        self.vl_pub.publish(Float64(vr))
+
+def main(args = None):
+    rclpy.init(args = args)
+
+    node = VelocityTranslator()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+
+    rclpy.shutdown()
+
+if __name__ == main():
+    main()
